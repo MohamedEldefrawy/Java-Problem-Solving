ba9c2ddd12535e8a49672aab4c94e4cda3d03e36
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-25 00:55:17 +0200

    Solve RemoveDuplicatesFromLinkedList

---
+++ src/RemoveDuplicatesFromSortedLinkedList.java
@@ -0,0 +1,50 @@
+public class RemoveDuplicatesFromSortedLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode left = head;
+        ListNode right = head;
+
+        while (left != null) {
+
+            if (right.next == null) {
+                left = left.next;
+                right = left;
+                continue;
+            }
+
+            if (left.val == right.next.val) {
+                right.next = right.next.next;
+            } else {
+                right = right.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1,
+                new ListNode(2,
+                        new ListNode(1,
+                                new ListNode(3, new ListNode(1, null)))));
+
+        ListNode result = deleteDuplicates(node);
+        System.out.println(result.val);
+        System.out.println(result.next.val);
+    }
+}

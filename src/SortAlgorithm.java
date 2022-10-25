df4736a0f64785ce184d44072adfe2f988df1d14
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-25 16:32:57 +0200

    Solve bubble sort

---
+++ src/SortAlgorithm.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class SortAlgorithm {
+    public static void main(String[] args) {
+        int[] input = new int[]{5, 4, 3, 2, 0, 0, 1};
+        System.out.println("Selection sort: " + Arrays.toString(selectionSort(input)));
+        System.out.println("Selection sort: " + Arrays.toString(bubbleSort(input)));
+    }
+
+    public static int[] selectionSort(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    swap(numbers, i, j);
+                }
+            }
+        }
+        return numbers;
+    }
+
+    public static int[] bubbleSort(int[] numbers) {
+        boolean sorted = false;
+        int pass = 1;
+        while (!sorted) {
+            for (int i = 0; i < numbers.length - pass; i++) {
+                if (numbers[i] > numbers[i + 1]) {
+                    swap(numbers, numbers[i], numbers[i + 1]);
+                    sorted = false;
+                } else {
+                    sorted = true;
+                }
+            }
+            pass++;
+        }
+        return numbers;
+    }
+
+    private static void swap(int[] numbers, int i, int j) {
+        int temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+}
